a18e4521343d7ab340c7dd90324a2d48e48819c5
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-05-18 10:56:55 +0530

    Added fibonaaci patterns and prime number patterns

---
+++ src/Practice2/Fibonaaci.java
@@ -0,0 +1,20 @@
+package Practice2;
+
+public class Fibonaaci {
+    public static void fib(int count) {
+        int a=0, b=1;
+        System.out.print(a + " " + b + " ");
+        for(int i=2; i<=count; i++) {
+            int sum = a+b;
+            System.out.print(sum + " ");
+            a=b;
+            b=sum;
+
+        }
+    }
+
+
+    public static void main(String[] args) {
+        fib(5);
+    }
+}
--- src/Practice2/PrimeNumbers.java
+++ src/Practice2/PrimeNumbers.java
@@ -1,4 +1,32 @@
 package Practice2;
 
 public class PrimeNumbers {
+    public static boolean isPrime(int n){
+
+        //A prime number is defined as a number greater than 1 that has no divisors other than 1 and itself. To check if
+        //n is prime, you need to see if it has any divisors other than 1 and
+        //𝑛
+        //n
+        if (n <= 1){
+            return false;
+        }
+
+        for (int i=2; i<= Math.sqrt(n); i++){
+            if (n % i == 0){ //if divisors available, its not prime
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+       int n = 2;
+       if (isPrime(n)){
+           System.out.println(n + " is a prime number");
+       }
+
+       else{
+           System.out.println(n + " is not a prime number");
+       }
+    }
 }
