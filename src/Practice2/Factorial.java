b82afd5066ca8ab7f860065e334577a1c7d882ac
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-05-18 11:51:21 +0530

    factorial done

---
+++ src/Practice2/Factorial.java
@@ -0,0 +1,17 @@
+package Practice2;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(factorial(n));
+
+    }
+
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+}
