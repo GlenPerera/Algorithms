7789b3eadb5979b2964ca9174f4e886fb13ba944
Glen Perera <123937629+jumangl@users.noreply.github.com>
2024-05-25 07:37:39 +0530

    Added Leetcode questions package

---
+++ src/LeetCodeQuestions/twoSum.java
@@ -0,0 +1,24 @@
+package LeetCodeQuestions;
+
+import java.util.Arrays;
+
+public class twoSum {
+    public int[] TwoSum(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int [] nums = {1,3,4,5,6,7,8};
+        int target = 9;
+
+    }
+
+}
